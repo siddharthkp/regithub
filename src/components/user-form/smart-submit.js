883a9ef7bdf93ca8c7461eacef1e2532a63c3929
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-10-27 18:44:13 +0530

    refactor smart submit

--- src/components/user-form/smart-submit.js
+++ src/components/user-form/smart-submit.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-class SmartSubmit extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { value: '', disabled: true }
+const SmartSubmit = props => {
+  const [disabled, setDisabled] = useState(true)
+
+  const onChange = event => {
+    setDisabled(event.target.value.length === 0)
   }
-  onChange = event => {
-    this.setState({ disabled: event.target.value.length === 0 })
-  }
-  render() {
-    return this.props.children({
-      disabled: this.state.disabled,
-      onChange: this.onChange
-    })
-  }
+
+  return props.children({ disabled, onChange })
 }
+
+// class SmartSubmit extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = { disabled: true }
+//   }
+//   onChange = event => {
+//     this.setState({ disabled: event.target.value.length === 0 })
+//   }
+//   render() {
+//     return this.props.children({
+//       disabled: this.state.disabled,
+//       onChange: this.onChange
+//     })
+//   }
+// }
 
 export default SmartSubmit
