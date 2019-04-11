391348a6a137ece3160c983c0ad47efb39b54074
siddharthkp <siddharth.kshetrapal@gmail.com>
2019-04-12 00:06:35 +0200

    add stub

---
+++ src/user.stub.js
@@ -0,0 +1,15 @@
+const user = {
+  username: 'sid',
+  name: { first: 'Sid', last: 'Kshetrapal' },
+  firstName: 'Sid',
+  fullName: 'Siddharth Kshetrapal',
+  email: 'siddharth.kshetrapal@gmail.com',
+  social: {
+    twitter: '@siddharthkp',
+    website: 'https://sid.studio'
+  },
+  joined: '2019-04-10',
+  verified: false
+}
+
+export default user
