441184b954f39dad975457b7de7e93a593234641
siddharthkp <siddharth.kshetrapal@gmail.com>
2019-05-17 13:02:19 +0530

    change name in gtest

--- src/form.test.js
+++ src/form.test.js
@@ -1,22 +1,26 @@
 import React from 'react'
-import LoginForm from './pages/login'
 import user from './user.stub'
+import LoginForm from './pages/login'
 
 import { render, fireEvent, cleanup } from 'react-testing-library'
 afterEach(cleanup)
 
-test('Button should be disabled by default', () => {
-  const component = render(<LoginForm user={user} />)
-  const button = component.getByText('Log in')
-  expect(button.disabled).toBe(true)
+test('lol wut, tests', ({ fail }) => {
+  fail()
 })
 
-test('Button should become enabled when input is given', () => {
-  const component = render(<LoginForm user={user} />)
-
-  const input = component.getByPlaceholderText('enter your password')
-  fireEvent.change(input, { target: { value: 'a' } })
-
-  const button = component.getByText('Log in')
-  expect(button.disabled).toBe(false)
-})
+// test('Button should be disabled by default', () => {
+//   const component = render(<LoginForm user={user} />)
+//   const button = component.getByText('Log in')
+//   expect(button.disabled).toBe(true)
+// })
+//
+// test('Button should become enabled when input is given', () => {
+//   const component = render(<LoginForm user={user} />)
+//
+//   const password = component.getByPlaceholderText('enter your password')
+//   fireEvent.change(password, { target: { value: 'a' } })
+//
+//   const button = component.getByText('Log in')
+//   expect(button.disabled).toBe(false)
+// })
