b3200a0be8d74d82a4003379b5458d29de727c2c
siddharthkp <siddharth.kshetrapal@gmail.com>
2019-05-27 11:18:53 +0530

    add travis.yml

--- src/form.test.js
+++ src/form.test.js
@@ -5,36 +5,20 @@ import LoginForm from './pages/login'
 import { render, fireEvent, cleanup } from 'react-testing-library'
 afterEach(cleanup)
 
-test('lol wut, tests', ({ fail }) => {
-  fail()
+test('Button should be disabled by default', () => {
+  const component = render(<LoginForm user={user} />)
+  const button = component.getByText('Log in')
+  expect(button.disabled).toBe(true)
 })
 
-/*
+test('Button should become enabled when input is given', () => {
+  const component = render(<LoginForm user={user} />)
 
+  const password = component.getByPlaceholderText(
+    'enter your password'
+  )
+  fireEvent.change(password, { target: { value: 'a' } })
 
-
-
-
-
-
-
-
-
-
-*/
-
-// test('Button should be disabled by default', () => {
-//   const component = render(<LoginForm user={user} />)
-//   const button = component.getByText('Log in')
-//   expect(button.disabled).toBe(true)
-// })
-//
-// test('Button should become enabled when input is given', () => {
-//   const component = render(<LoginForm user={user} />)
-//
-//   const password = component.getByPlaceholderText('enter your password')
-//   fireEvent.change(password, { target: { value: 'a' } })
-//
-//   const button = component.getByText('Log in')
-//   expect(button.disabled).toBe(false)
-// })
+  const button = component.getByText('Log in')
+  expect(button.disabled).toBe(false)
+})
