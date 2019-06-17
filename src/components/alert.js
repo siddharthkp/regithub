da0bef1624bd54e95131b6baf1d50466b6d9ced7
siddharthkp <siddharth.kshetrapal@gmail.com>
2019-06-17 18:50:59 +0530

    onlu alert

--- src/components/alert.js
+++ src/components/alert.js
@@ -5,4 +5,10 @@ function Alert(props) {
   return <div className="alert">{props.message}</div>
 }
 
+// Alert.propTypes = {
+//   children: function(props) {
+//     if (props.children) return new Error('kids not allowed')
+//   }
+// }
+
 export default Alert
